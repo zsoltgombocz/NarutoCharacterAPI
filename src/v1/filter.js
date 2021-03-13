74407e2fe643f08c40f496712d0b63fca34c2306
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2021-03-13 21:55:29 +0100

    Characters file checking

--- src/v1/filter.js
+++ src/v1/filter.js
@@ -1,4 +1,23 @@
-const Characters = require('../../Characters.json');
+const fs = require('fs');
+const path = require('path');
+
+const root = path.dirname(require.main.filename || process.mainModule.filename);
+let Characters;
+try {
+  if(fs.existsSync(root + '/scraper/Characters.json')) {
+      Characters = require(root + '/scraper/Characters.json');
+  } else {
+    console.log('Could not find Characters.json! Creatinga new one in src/scraper/ directory...')
+
+    fs.writeFileSync(root + '/scraper/Characters.json', "[]", err =>{
+      console.log(err)
+    })
+    Characters = require(root + '/scraper/Characters.json');
+  }
+} catch (err) {
+  console.error(err);
+}
+
 const Fuse = require('fuse.js')
 
 async function filterArray(options = null) {
