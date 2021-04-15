e84116cbba1d54ac73da7ebe8d2c06b782068011
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-15 14:29:06 +0200

    Added random and populars routes

--- src/v1/index.js
+++ src/v1/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const file = require('../file.js');
+const Populars = require(file.getRoot() + '/scraper/Populars.json')
 
 const {filterArray} = require('./filter');
 
@@ -11,8 +13,28 @@ router.get('/', async (req, res) => {
     count: data.length,
     data: data
   });
-
-
 });
 
+router.get('/populars', async (req, res) => {
+  res.json({
+    count: Populars.length,
+    data: Populars
+  });
+});
+
+router.get('/random', async (req, res) =>{
+  const data = await filterArray(null);
+
+  const limit = (req.query.limit != undefined) ? req.query.limit : 1;
+  let randomCharacters = [];
+
+  for(i = 0; i < limit; i++){
+    randomCharacters.push(data[Math.floor(Math.random() * data.length)]);
+  }
+
+  res.json({
+    count: randomCharacters.length,
+    data: randomCharacters
+  });
+});
 module.exports = router;
