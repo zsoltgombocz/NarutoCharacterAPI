54ead7a61aa6e9f621dea1fc18c1f93a3f462c07
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-04-22 14:00:27 +0200

    Simplified code by created random function

--- src/v1/index.js
+++ src/v1/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const functions = require('./functions.js');
+
 const file = require('../file.js');
-const Populars = require(file.getRoot() + '/scraper/Populars.json')
+
+let Characters = (file.checkFile('scraper/Characters.json', true))? require(file.getRoot() + '/scraper/Characters.json') : process.exit(0);
+let Populars = (file.checkFile('scraper/Populars.json', true))? require(file.getRoot() + '/scraper/Populars.json') : process.exit(0);
 
 const {filterArray} = require('./filter');
+const { contentSecurityPolicy } = require('helmet');
 
 const router = express.Router();
 
@@ -16,41 +21,26 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/populars', async (req, res) => {
-  let PopularCharacters = []
-  for(let i = 0; i < Populars.length; i++){
-    const data = await filterArray({name: Populars[i]});
-    PopularCharacters.push(data[0]);
-  }
 
-
-  res.json({
-    count: PopularCharacters.length,
-    data: PopularCharacters
-  });
-});
-
-router.get('/random', async (req, res) =>{
-  const data = await filterArray(null);
-
-  const limit = (req.query.limit != undefined) ? req.query.limit : 1;
-  const img = (req.query.image != undefined) ? req.query.image : false;
-
-  let randomCharacters = [];
-
-  for(i = 0; i < limit; i++){
-    let ind = Math.floor(Math.random() * data.length);
-    if(img){
-      while(data[ind].image == undefined){
-        ind = Math.floor(Math.random() * data.length);
-      }
-      randomCharacters.push(data[ind]);
-    }else randomCharacters.push(data[ind]);
-   
-  }
+  let randomCharacters = functions.random(Populars, req.query)
 
   res.json({
     count: randomCharacters.length,
     data: randomCharacters
   });
 });
+
+router.get('/random', async (req, res) =>{
+
+  let randomCharacters = functions.random(Characters, req.query)
+
+  res.json({
+    count: randomCharacters.length,
+    data: randomCharacters
+  });
+});
+
+
+
+
 module.exports = router;
