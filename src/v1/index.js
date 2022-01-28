ae9d31cac3d31c6406b8ad7c8023d1a89fb456d5
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-01-28 20:15:39 +0100

    Added search query and replaced array with db select

--- src/v1/index.js
+++ src/v1/index.js
@@ -1,46 +1,57 @@
 const express = require('express');
 const functions = require('./functions.js');
 
-const file = require('../file.js');
-
-let Characters = (file.checkFile('scraper/Characters.json', true))? require(file.getRoot() + '/scraper/Characters.json') : process.exit(0);
-let Populars = (file.checkFile('scraper/Populars.json', true))? require(file.getRoot() + '/scraper/Populars.json') : process.exit(0);
-
-const {filterArray} = require('./filter');
+const {createSearchOptions} = require('./filter');
 const { contentSecurityPolicy } = require('helmet');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  const data = await filterArray(req.query);
-  
-  res.json({
-    count: data.length,
-    data: data
+let CharacterModel = require('../database/models/Character')
+let PopularModel = require('../database/models/Popular')
+
+router.get('/:query', async (req, res) => {
+  let data = [];
+  const query = await createSearchOptions(req.params.query)
+
+  await CharacterModel.find(query).cursor().
+  on('data', function(doc) { data.push(doc) }).
+  on('end', function() { 
+    res.json({
+      count: data.length,
+      data: data
+    });
   });
 });
 
 router.get('/populars', async (req, res) => {
+  let data = [];
+  let randomCharacters = [];
+  await PopularModel.find().cursor().
+  on('data', function(doc) { data.push(doc) }).
+  on('end', function() { 
+   randomCharacters = functions.random(data, req.query);
 
-  let randomCharacters = functions.random(Populars, req.query)
-
-  res.json({
-    count: randomCharacters.length,
-    data: randomCharacters
+    res.json({
+      count: randomCharacters.length,
+      data: randomCharacters
+    });
   });
 });
 
 router.get('/random', async (req, res) =>{
+  let data = [];
+  let randomCharacters = [];
+  await CharacterModel.find().cursor().
+  on('data', function(doc) { data.push(doc) }).
+  on('end', function() { 
+   randomCharacters = functions.random(data, req.query);
 
-  let randomCharacters = functions.random(Characters, req.query)
-
-  res.json({
-    count: randomCharacters.length,
-    data: randomCharacters
+    res.json({
+      count: randomCharacters.length,
+      data: randomCharacters
+    });
   });
 });
-
-
 
 
 module.exports = router;
