be9cf9c828aded5a38b1cfe1232ee86ad95f0c70
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-01-28 20:12:26 +0100

    removed fuse.js and added basic mongoose query create from url params

--- src/v1/filter.js
+++ src/v1/filter.js
@@ -1,53 +1,29 @@
-const file = require('../file.js');
+async function createSearchOptions(options = null) {
+   if(options === null || options === undefined || options === '' || Object.keys(options).length === 0) return array;
 
-let Characters = (file.checkFile('scraper/Characters.json', true))? require(file.getRoot() + '/scraper/Characters.json') : process.exit(0);
-
-
-const Fuse = require('fuse.js')
-
-async function filterArray(options = null) {
-   if(options === null || options === undefined || options === '' || Object.keys(options).length === 0) return Characters;
-
-    let args = []
-    const fuse_options = {
-        // isCaseSensitive: false,
-        // includeScore: false,
-        // shouldSort: true,
-        // includeMatches: false,
-        findAllMatches: false,
-        // minMatchCharLength: 1,
-        // location: 0,
-        threshold: 0.3,
-        // distance: 100,
-        // useExtendedSearch: false,
-        // ignoreLocation: false,
-        // ignoreFieldNorm: false,
-        keys: [
-          "name",
-          "personal.sex",
-          "personal.affiliation",
-          "personal.team",
-        ]
-      };
-      for(option in options) {
-          obj = {}
-          if(typeof options[option] === "object") {
-              for(val in  options[option]) {
-                obj = {}
-                const key = option + "." + val
-                obj[key] = "'" + options[option][val]
-                args.push(obj)
-              }
-          }else{
-            obj[option] = "'" + options[option]
-            args.push(obj)
-          }        
+    let arrayOfOptions = options.split('&');
+    let constructedFilter = {};
+    for(const option of arrayOfOptions) {
+      
+      if(option.includes("=")){
+          const key = option.split("=")[0];
+          const value = option.split("=")[1];
+          constructedFilter[key] = {$regex: value, $options: 'i'}
       }
+    }
+
+    console.log(constructedFilter)
+
+    return constructedFilter
       
-      const fuse = new Fuse(Characters, fuse_options);
-      
-      return await fuse.search({$and: args})
+      /*let data = [];
+      console.log(req.params)
+      await CharacterModel.find(req.query).cursor().
+      on('data', function(doc) { data.push(doc) }).
+      on('end', function() { 
+        return data;  
+      });  */
 
 }
 
-module.exports = {filterArray};
+module.exports = {createSearchOptions};
